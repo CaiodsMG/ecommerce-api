7bdefe01978b3efbd353647a1a6f50eeaa63794b
CaiodsMG <caiocfn10@gmail.com>
2026-03-12 14:27:55 -0300

    Criação da Entidade Produto

---
+++ src/main/java/com/ecommerce/entity/Produto.java
@@ -0,0 +1,29 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produtos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer estoque;
+}
