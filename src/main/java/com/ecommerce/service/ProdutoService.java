c4b0a2ef5c9f9eb1717f8b800b0172672b62fab2
CaiodsMG <caiocfn10@gmail.com>
2026-03-12 14:48:28 -0300

    Implementado service de produto

---
+++ src/main/java/com/ecommerce/service/ProdutoService.java
@@ -0,0 +1,47 @@
+package com.ecommerce.service;
+
+import com.ecommerce.entity.Produto;
+import com.ecommerce.repository.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired //Injeção de dependência
+    private ProdutoRepository repository;
+
+    //Criação de um produto
+    public Produto criar(Produto produto){
+        return repository.save(produto);
+    }
+
+    //Listar todos os produtos
+    public List<Produto> listar(){
+        return repository.findAll();
+    }
+
+    //Procura o produto pelo id informado, se o id não existir retornará uma mensagem.
+    public Produto procurarPorId(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("O produto com o id: " + id + " não existe."));
+    }
+
+    //Procura o produto pelo id informado e pede um corpo para a atualização.
+    public Produto atualizar(Long id, Produto produto){
+        Produto produtoAtualizado = procurarPorId(id);
+
+        BeanUtils.copyProperties(produto, produtoAtualizado, "id");
+
+        return repository.save(produtoAtualizado);
+    }
+
+    //Deleta o produto pelo id
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
